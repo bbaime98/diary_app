8c74b3f2f678bc16f81e28658eaa64206f3e50b1
bien aime <bybienaime@gmmail.com>
2020-10-04 18:15:40 +0200

    add delete funcitonality
     - add delete action
     - add delete handler to the delete icon

--- app/redux/actions/postsActions.js
+++ app/redux/actions/postsActions.js
@@ -1,21 +1,51 @@
 import axios from "axios"
-import {GET_POSTS_ERROR, GET_POSTS_SUCCESS} from "./actionTypes"
+import {
+  GET_POSTS_ERROR,
+  GET_POSTS_SUCCESS,
+  DELETE_POST_ERROR,
+  DELETE_POST_SUCCESS,
+  REDIRECT_USER,
+} from "./actionTypes"
 import authStorage from "../../utils/storage"
 
 export const getPostsAction = () => async (dispatch) => {
   try {
     const token = await authStorage.getToken()
+    // console.log("GET POSTS____TOKEN", token)
+    if (!token) {
+      return dispatch({type: REDIRECT_USER, payload: false})
+    }
     const res = await axios.get(
       "https://mydiary-api-app.herokuapp.com/api/v2/entries",
       {headers: {token}}
     )
     const {data} = res.data
-    // console.log("ACTIONSS+++++++++", data)
 
     return dispatch({type: GET_POSTS_SUCCESS, payload: data})
   } catch (error) {
-    console.log("Action error", error)
+    console.log("GET POSTS___Action error", error)
 
     return dispatch({type: GET_POSTS_ERROR, payload: error.response.data.error})
+  }
+}
+
+export const deletePostAction = (id) => async (dispatch) => {
+  try {
+    const token = await authStorage.getToken()
+    const res = await axios.delete(
+      `https://mydiary-api-app.herokuapp.com/api/v2/entries/${id}`,
+      {headers: {token}}
+    )
+    console.log("ACTIONSS+++++++++", res.data)
+    const {data} = res.data
+
+    return dispatch({type: DELETE_POST_SUCCESS, payload: data})
+  } catch (error) {
+    console.log("Action error delete___________-", error)
+
+    return dispatch({
+      type: DELETE_POST_ERROR,
+      payload: error.response.data.error,
+    })
   }
 }
--- app/redux/reducers/PostsReducer.js
+++ app/redux/reducers/PostsReducer.js
@@ -1,10 +1,19 @@
-import {GET_POSTS_ERROR, GET_POSTS_SUCCESS} from "../actions/actionTypes"
+import {
+  GET_POSTS_ERROR,
+  GET_POSTS_SUCCESS,
+  DELETE_POST_ERROR,
+  DELETE_POST_SUCCESS,
+} from "../actions/actionTypes"
 
 export default (state = {data: null, error: null}, action) => {
   switch (action.type) {
     case GET_POSTS_SUCCESS:
       return {...state, data: action.payload}
     case GET_POSTS_ERROR:
+      return {...state, error: action.payload}
+    case DELETE_POST_SUCCESS:
+      return {...state, data: action.payload}
+    case DELETE_POST_ERROR:
       return {...state, error: action.payload}
     default:
       return state
